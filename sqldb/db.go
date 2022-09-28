7337c7f35ef471904f5e5afcd2a2b68bfec129f5
Konstantinos Vlachopoulos <kon@vlach.gr>
2022-09-28 13:14:59 +0300

    sqldb: docs, move prefix explanation to the top

--- sqldb/db.go
+++ sqldb/db.go
@@ -15,6 +15,13 @@ import (
 // supported is creating the table and getting a list of prefixes for the
 // current scope. The rest (adding, deleting, etc.) is handled externally by a
 // command.
+//
+// The idea of having the ability to have an arbitrary number of prefixes
+// originated from the fact that I wanted to not have to use a prefix when
+// DMing the bot, as it is pointless to do so in that case. So after building
+// support for more than 1 prefix it felt unecessary to limit it to just DMs,
+// as that would be an artificial limit on what the bot is already supports and
+// not really a necessity.
 
 const schema = `
 CREATE TABLE IF NOT EXISTS Scopes (
@@ -110,13 +117,6 @@ func (_ *DB) ScopeAdd(tx *sql.Tx) (int64, error) {
 }
 
 // Returns the list of all prefixes for a specific scope.
-//
-// The idea of having the ability to have an arbitrary number of prefixes
-// originated from the fact that I wanted to not have to use a prefix when
-// DMing the bot, as it is pointless to do so in that case. So after building
-// support for more than 1 prefix it felt unecessary to limit it to just DMs,
-// as that would be an artificial limit on what the bot is already supports and
-// not really a necessity.
 func (db *DB) PrefixList(scope int64) ([]string, error) {
 	db.Lock.RLock()
 	defer db.Lock.RUnlock()
