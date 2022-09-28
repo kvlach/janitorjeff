f765f6da9298673efc1473a066bf9ea2e8df127e
Konstantinos Vlachopoulos <kon@vlach.gr>
2022-09-28 14:19:12 +0300

    core: Add documentation for MatchCommand

--- core/commands.go
+++ core/commands.go
@@ -28,6 +28,15 @@ func (cmds *Commands) matchCommand(cmdName string) (*CommandStatic, error) {
 	return nil, fmt.Errorf("command '%s' not found", cmdName)
 }
 
+// Given a list of arguments match the appropriate command. The arguments don't
+// have to correspond to a command exactly. For example,
+//
+// `args = [prefix add abc]`.
+//
+// In this case the prefix's subcommand `add` will be matched and returned.
+// Alongside it the index of the last valid command will be returned (in this
+// case the index of "add", which is 1). This makes it easy to know which
+// aliases where used by the user when invoking a command.
 func (cmds *Commands) MatchCommand(args []string) (*CommandStatic, int, error) {
 	log.Debug().Strs("args", args).Msg("trying to match command")
 
