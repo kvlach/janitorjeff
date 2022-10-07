b9178d22cdc102797ef175f163b2d77a208f0a54
Konstantinos Vlachopoulos <kon@vlach.gr>
2022-10-07 13:28:36 +0300

    Add some early support for writing tests
    Makes it easy to create and delete a temporary database and generate a
    random message.

---
+++ internal/testkit/testkit.go
@@ -0,0 +1,99 @@
+package testkit
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"git.slowtyper.com/slowtyper/janitorjeff/core"
+	"git.slowtyper.com/slowtyper/janitorjeff/platforms/discord"
+	"git.slowtyper.com/slowtyper/janitorjeff/sqldb"
+
+	dg "github.com/bwmarrin/discordgo"
+)
+
+type TestDB struct {
+	name string
+	db   *sqldb.DB
+}
+
+func NewTestDB(name string) *TestDB {
+	name = fmt.Sprintf("%d-%s", time.Now().UnixNano(), name)
+
+	db, err := sqldb.Open("sqlite3", name)
+	if err != nil {
+		log.Fatalf("failed to open DB: %v\n", err)
+	}
+
+	globals := &core.GlobalVars{
+		DB: db,
+	}
+	core.GlobalsInit(globals)
+
+	tdb := &TestDB{
+		name: name,
+		db:   db,
+	}
+	return tdb
+}
+
+func (tdb *TestDB) Delete() {
+	err := os.Remove(tdb.name)
+	if err != nil {
+		log.Fatalf("failed to delete DB: %v\n", err)
+	}
+}
+
+func (tdb *TestDB) Schema(schema string) error {
+	return tdb.db.Init(schema)
+}
+
+type TestMessage struct {
+	core.Message
+}
+
+var names = []string{
+	"janitor",
+	"jeff",
+	"janitorjeff",
+	"JanitorJeff",
+}
+
+func NewTestMessage() *TestMessage {
+	return &TestMessage{core.Message{}}
+}
+
+func randomID() string {
+	return fmt.Sprintf("%d", time.Now().UnixNano())
+}
+
+func randomName() string {
+	rand.Seed(time.Now().UnixNano())
+	return names[rand.Intn(len(names))]
+}
+
+func (tm *TestMessage) DiscordRandom() *TestMessage {
+	dgMsg := &dg.MessageCreate{
+		Message: &dg.Message{
+			ID:      randomID(),
+			Content: "tmp",
+			Author: &dg.User{
+				ID:       randomID(),
+				Username: randomName(),
+			},
+			ChannelID: randomID(),
+			GuildID:   "123", // isDM
+		},
+	}
+
+	msgTmp := discord.DiscordMessageCreate{Session: nil, Message: dgMsg}
+
+	msg, err := msgTmp.Parse()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return &TestMessage{*msg}
+}
