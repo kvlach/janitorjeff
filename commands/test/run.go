a6da89d26971c4288498e1563c85d6ac9841e1a3
Konstantinos Vlachopoulos <kon@vlach.gr>
2022-10-11 11:09:44 +0300

    Add a simple test command

---
+++ commands/test/run.go
@@ -0,0 +1,31 @@
+package test
+
+import (
+	"git.slowtyper.com/slowtyper/janitorjeff/core"
+
+	dg "github.com/bwmarrin/discordgo"
+)
+
+func run(m *core.Message) (interface{}, error, error) {
+	switch m.Type {
+	case core.Discord:
+		return run_Discord(m)
+	default:
+		return run_Text(m)
+	}
+}
+
+func run_Discord(m *core.Message) (*dg.MessageEmbed, error, error) {
+	embed := &dg.MessageEmbed{
+		Description: run_Core(),
+	}
+	return embed, nil, nil
+}
+
+func run_Text(m *core.Message) (string, error, error) {
+	return m.ReplyText(run_Core()), nil, nil
+}
+
+func run_Core() string {
+	return "Test command!"
+}
