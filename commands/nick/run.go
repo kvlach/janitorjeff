5affe3548568728df1ea5f6118c5789671da6526
Konstantinos Vlachopoulos <kon@vlach.gr>
2022-10-20 11:35:58 +0300

    cmd/nick: Add core run functions

---
+++ commands/nick/run.go
@@ -0,0 +1,34 @@
+package nick
+
+func runGet(person, place int64) (string, error, error) {
+	exists, err := dbUserExists(person, place)
+	if err != nil {
+		return "", nil, err
+	}
+	if !exists {
+		return "", errUserNotFound, nil
+	}
+
+	nick, err := dbUserNick(person, place)
+	return nick, nil, err
+}
+
+func runSet(nick string, person, place int64) (string, error, error) {
+	nickExists, err := dbNickExists(nick, place)
+	if err != nil {
+		return "", nil, err
+	}
+	if nickExists {
+		return nick, errNickExists, nil
+	}
+
+	personExists, err := dbUserExists(person, place)
+	if err != nil {
+		return "", nil, err
+	}
+
+	if personExists {
+		return nick, nil, dbUserUpdate(person, place, nick)
+	}
+	return nick, nil, dbUserAdd(person, place, nick)
+}
