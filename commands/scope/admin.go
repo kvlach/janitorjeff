bb6d0e2aeeb2ce5efd905aaa3d1e0db11af882bd
Konstantinos Vlachopoulos <kon@vlach.gr>
2022-10-21 12:52:30 +0300

    Add scope admin command

---
+++ commands/scope/admin.go
@@ -0,0 +1,102 @@
+package scope
+
+import (
+	"fmt"
+
+	"git.slowtyper.com/slowtyper/janitorjeff/core"
+)
+
+var Admin = &core.CommandStatic{
+	Names: []string{
+		"scope",
+	},
+	Description: "Scope related commands.",
+	UsageArgs:   "(place | person)",
+	Run:         adminRun,
+
+	Children: core.Commands{
+		{
+			Names: []string{
+				"place",
+			},
+			Description: "Find a places's scope.",
+			UsageArgs:   "<id>",
+			Run:         adminRunPlace,
+		},
+		{
+			Names: []string{
+				"person",
+			},
+			Description: "Find a person's scope.",
+			UsageArgs:   "<id> [parent]",
+			Run:         adminRunPerson,
+		},
+	},
+}
+
+func adminRun(m *core.Message) (any, error, error) {
+	return m.ReplyUsage(), core.ErrMissingArgs, nil
+}
+
+///////////
+//       //
+// place //
+//       //
+///////////
+
+func adminRunPlace(m *core.Message) (any, error, error) {
+	if len(m.Command.Runtime.Args) < 1 {
+		return m.ReplyUsage(), core.ErrMissingArgs, nil
+	}
+	place, err := adminRunPlaceCore(m)
+	return fmt.Sprint(place), nil, err
+}
+
+func adminRunPlaceCore(m *core.Message) (int64, error) {
+	target := m.Command.Runtime.Args[0]
+	return runPlace(target, m.Client)
+}
+
+////////////
+//        //
+// person //
+//        //
+////////////
+
+func adminRunPerson(m *core.Message) (any, error, error) {
+	if len(m.Command.Runtime.Args) < 1 {
+		return m.ReplyUsage(), core.ErrMissingArgs, nil
+	}
+
+	switch m.Type {
+	case core.Discord:
+		return adminRunPersonParent(m)
+	case core.Twitch:
+		return adminRunPersonNoParent(m)
+	default:
+		return "This command doesn't currently support this frontend.", nil, nil
+	}
+}
+
+func adminRunPersonParent(m *core.Message) (any, error, error) {
+	if len(m.Command.Runtime.Args) < 2 {
+		return m.ReplyUsage(), core.ErrMissingArgs, nil
+	}
+
+	target := m.Command.Runtime.Args[0]
+	parent := m.Command.Runtime.Args[1]
+
+	person, err := runPerson(target, parent, m.Client)
+	return fmt.Sprint(person), nil, err
+}
+
+func adminRunPersonNoParent(m *core.Message) (any, error, error) {
+	if len(m.Command.Runtime.Args) < 2 {
+		return m.ReplyUsage(), core.ErrMissingArgs, nil
+	}
+
+	target := m.Command.Runtime.Args[0]
+
+	person, err := runPerson(target, "", m.Client)
+	return fmt.Sprint(person), nil, err
+}
---
+++ commands/scope/core.go
@@ -0,0 +1,27 @@
+package scope
+
+import (
+	"git.slowtyper.com/slowtyper/janitorjeff/core"
+)
+
+func runPlace(target string, client core.Messenger) (int64, error) {
+	id, err := client.PlaceID(target)
+	if err != nil {
+		return -1, err
+	}
+	return client.PlaceScope(id)
+}
+
+func runPerson(target, parent string, client core.Messenger) (int64, error) {
+	placeID, err := client.PlaceID(parent)
+	if err != nil {
+		return -1, err
+	}
+
+	id, err := client.PersonID(target, placeID)
+	if err != nil {
+		return -1, err
+	}
+
+	return client.PersonScope(id)
+}
