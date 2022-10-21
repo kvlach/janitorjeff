981f1f1771d338f2457a73e3f860bc483bc71dbd
Konstantinos Vlachopoulos <kon@vlach.gr>
2022-10-21 12:52:30 +0300

    Add scope admin command

--- commands/commands.go
+++ commands/commands.go
@@ -5,6 +5,7 @@ import (
 	"git.slowtyper.com/slowtyper/janitorjeff/commands/help"
 	"git.slowtyper.com/slowtyper/janitorjeff/commands/nick"
 	"git.slowtyper.com/slowtyper/janitorjeff/commands/prefix"
+	"git.slowtyper.com/slowtyper/janitorjeff/commands/scope"
 	"git.slowtyper.com/slowtyper/janitorjeff/commands/test"
 	"git.slowtyper.com/slowtyper/janitorjeff/commands/time"
 	"git.slowtyper.com/slowtyper/janitorjeff/core"
@@ -26,5 +27,6 @@ var Advanced = core.Commands{
 var Admin = core.Commands{
 	nick.Admin,
 	prefix.Admin,
+	scope.Admin,
 	test.Command,
 }
