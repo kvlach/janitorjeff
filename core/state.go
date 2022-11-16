bfe5af8e3c44c252feceddee0fe7a9e626cabfd8
Konstantinos Vlachopoulos <kon@vlach.gr>
2022-11-16 18:07:49 +0200

    core: Use gosafe for States struct

--- core/state.go
+++ core/state.go
@@ -3,56 +3,29 @@ package core
 import (
 	"crypto/rand"
 	"encoding/base64"
-	"sync"
+
+	"git.slowtyper.com/slowtyper/gosafe"
 )
 
 // OAuth state parameter
-
 type States struct {
-	lock   sync.Mutex
-	tokens []string
-}
-
-func (s *States) Add(token string) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-
-	s.tokens = append(s.tokens, token)
-}
-
-func (s *States) in(token string) bool {
-	for _, t := range s.tokens {
-		if t == token {
-			return true
-		}
-	}
-	return false
-}
-
-func (s *States) In(token string) bool {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-
-	return s.in(token)
+	gosafe.Slice[string]
 }
 
 func (s *States) Delete(token string) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
+	s.Lock()
+	defer s.Unlock()
 
-	for i, t := range s.tokens {
-		if t == token {
-			// delete the token by replacing it with the last one since the
-			// order doesn't matter
-			s.tokens[i] = s.tokens[len(s.tokens)-1]
-			s.tokens = s.tokens[:len(s.tokens)-1]
+	for i := 0; i < s.LenUnsafe(); i++ {
+		if s.GetUnsafe(i) == token {
+			s.DeleteUnstableUnsafe(i)
 		}
 	}
 }
 
 func (s *States) Generate() (string, error) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
+	s.Lock()
+	defer s.Unlock()
 
 	b := make([]byte, 32)
 	_, err := rand.Read(b)
@@ -62,7 +35,7 @@ func (s *States) Generate() (string, error) {
 	state := base64.URLEncoding.EncodeToString(b)
 
 	// you never know
-	if s.in(state) {
+	if s.InUnsafe(state) {
 		return s.Generate()
 	}
 	return state, nil
