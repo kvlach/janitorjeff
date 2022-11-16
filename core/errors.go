1fe80444fbfa368032f7ab7f6f369837c416c02f
Konstantinos Vlachopoulos <kon@vlach.gr>
2022-11-16 17:54:49 +0200

    core: Remove unused errors

--- core/errors.go
+++ core/errors.go
@@ -7,6 +7,4 @@ import (
 var (
 	ErrMissingArgs = errors.New("not enough arguments provided")
 	ErrSilence     = errors.New("if this error is returned don't send any message")
-	ErrNotMod      = errors.New("Mod permissions are required.")
-	ErrNotAdmin    = errors.New("Admin permissions are required.")
 )
