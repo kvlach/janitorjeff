7ab42ed89aae16489dfe07c5ec907621c778e796
Konstantinos Vlachopoulos <kon@vlach.gr>
2022-12-09 09:39:32 +0200

    cmd/nick: Add doc comments for core functions

--- commands/nick/core.go
+++ commands/nick/core.go
@@ -9,6 +9,8 @@ var (
 	errNickExists     = errors.New("nick is used by a different user")
 )
 
+// Show returns the person's nickname in the specified place. If no nickname
+// has been set then returns an errPersonNotFound error.
 func Show(person, place int64) (string, error, error) {
 	exists, err := dbPersonExists(person, place)
 	if err != nil {
@@ -22,6 +24,9 @@ func Show(person, place int64) (string, error, error) {
 	return nick, nil, err
 }
 
+// Set sets the person's nickname in the specified place. If the person has set
+// their nickname already then it updates it. If the nickname already exists in
+// that place then it returns an errNickExists error.
 func Set(nick string, person, place int64) (error, error) {
 	nickExists, err := dbNickExists(nick, place)
 	if err != nil {
@@ -42,6 +47,8 @@ func Set(nick string, person, place int64) (error, error) {
 	return nil, dbPersonAdd(person, place, nick)
 }
 
+// Delete deletes the person's nickname in the specified place. If no nickname
+// has been set then returns an errPersonNotFound error.
 func Delete(person, place int64) (error, error) {
 	exists, err := dbPersonExists(person, place)
 	if err != nil {
