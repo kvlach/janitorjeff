7c6051f7a6c150fde4580fb467b1709ed8421ee6
Konstantinos Vlachopoulos <kon@vlach.gr>
2022-12-21 11:25:21 +0200

    cmd/youtube: Add ability to search for channels
    Works exactly like the the video search functionality works

--- commands/youtube/api.go
+++ commands/youtube/api.go
@@ -9,11 +9,6 @@ import (
 	"google.golang.org/api/youtube/v3"
 )
 
-type Client struct {
-	key     string
-	service *youtube.Service
-}
-
 type Video struct {
 	ID    string
 	Title string
@@ -21,6 +16,20 @@ type Video struct {
 
 func (v Video) URL() string {
 	return "https://youtu.be/" + v.ID
+}
+
+type Channel struct {
+	ID    string
+	Title string
+}
+
+func (ch Channel) URL() string {
+	return "https://www.youtube.com/channel/" + ch.ID
+}
+
+type Client struct {
+	key     string
+	service *youtube.Service
 }
 
 // New returns a new youtube client using the key set in core.YouTubeKey.
@@ -61,4 +70,28 @@ func (c *Client) SearchVideos(query string, maxResults int64) ([]Video, error) {
 	}
 
 	return videos, nil
+}
+
+// SearchChannels returns a list of channels, of maxResults length, matching the
+// query and ranked by relevance.
+func (c *Client) SearchChannels(query string, maxResults int64) ([]Channel, error) {
+	call := c.service.Search.List([]string{"id", "snippet"}).
+		Q(query).
+		Type("channel").
+		MaxResults(maxResults)
+
+	response, err := call.Do()
+	if err != nil {
+		return nil, err
+	}
+
+	var chs []Channel
+	for _, item := range response.Items {
+		if item.Id.Kind == "youtube#channel" {
+			ch := Channel{item.Id.ChannelId, item.Snippet.ChannelTitle}
+			chs = append(chs, ch)
+		}
+	}
+
+	return chs, nil
 }
