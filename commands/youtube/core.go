b150d912a95760544c23ad178399e205af2b1b00
Konstantinos Vlachopoulos <kon@vlach.gr>
2022-12-21 11:25:21 +0200

    cmd/youtube: Add ability to search for channels
    Works exactly like the the video search functionality works

--- commands/youtube/core.go
+++ commands/youtube/core.go
@@ -4,7 +4,10 @@ import (
 	"errors"
 )
 
-var ErrVidNotFound = errors.New("No video was found.")
+var (
+	ErrVidNotFound     = errors.New("No video was found.")
+	ErrChannelNotFound = errors.New("No channel was found.")
+)
 
 // SearchVideo will automatically create a client and return the most relevant
 // completed video (not livestreams or premiers). Returns ErrVidNotFound if no
@@ -25,4 +28,23 @@ func SearchVideo(query string) (Video, error, error) {
 	}
 
 	return vids[0], nil, nil
+}
+
+// SearchChannel will automatically create a client and return the most relevant
+// channel. Returns ErrChannelNotFound if no channel was found.
+func SearchChannel(query string) (Channel, error, error) {
+	client, err := New()
+	if err != nil {
+		return Channel{}, nil, err
+	}
+
+	chs, err := client.SearchChannels(query, 1)
+	if err != nil {
+		return Channel{}, nil, err
+	}
+	if len(chs) == 0 {
+		return Channel{}, ErrChannelNotFound, nil
+	}
+
+	return chs[0], nil, nil
 }
