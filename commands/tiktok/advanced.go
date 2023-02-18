92b9011288696146634d99805fad63b0e6388de1
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-02-18 18:13:00 +0200

    cmd/tiktok: Add ability to customize voice per user

--- commands/tiktok/advanced.go
+++ commands/tiktok/advanced.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/janitorjeff/jeff-bot/commands/nick"
 	"github.com/janitorjeff/jeff-bot/core"
 	"github.com/janitorjeff/jeff-bot/frontends"
 
@@ -19,7 +20,7 @@ func (advanced) Type() core.CommandType {
 }
 
 func (advanced) Permitted(m *core.Message) bool {
-	return m.Speaker.Enabled()
+	return true
 }
 
 func (advanced) Names() []string {
@@ -44,11 +45,12 @@ func (advanced) Children() core.CommandsStatic {
 	return core.CommandsStatic{
 		AdvancedStart,
 		AdvancedStop,
+		AdvancedUser,
 	}
 }
 
 func (advanced) Init() error {
-	return nil
+	return core.DB.Init(dbSchema)
 }
 
 func (advanced) Run(m *core.Message) (any, error, error) {
@@ -70,7 +72,7 @@ func (c advancedStart) Type() core.CommandType {
 }
 
 func (c advancedStart) Permitted(m *core.Message) bool {
-	return c.Parent().Permitted(m)
+	return m.Speaker.Enabled()
 }
 
 func (c advancedStart) Names() []string {
@@ -145,7 +147,7 @@ func (c advancedStop) Type() core.CommandType {
 }
 
 func (c advancedStop) Permitted(m *core.Message) bool {
-	return c.Parent().Permitted(m)
+	return AdvancedStart.Permitted(m)
 }
 
 func (advancedStop) Names() []string {
@@ -212,4 +214,102 @@ func (c advancedStop) err(usrErr error) string {
 func (advancedStop) core(m *core.Message) error {
 	twitchUsername := strings.ToLower(m.Command.Args[0])
 	return Stop(twitchUsername)
+}
+
+//////////
+//      //
+// user //
+//      //
+//////////
+
+var AdvancedUser = advancedUser{}
+
+type advancedUser struct{}
+
+func (c advancedUser) Type() core.CommandType {
+	return c.Parent().Type()
+}
+
+func (c advancedUser) Permitted(m *core.Message) bool {
+	return c.Parent().Permitted(m) && m.Author.Mod()
+}
+
+func (advancedUser) Names() []string {
+	return []string{
+		"user",
+	}
+}
+
+func (advancedUser) Description() string {
+	return "Set a user's voice."
+}
+
+func (advancedUser) UsageArgs() string {
+	return "<user> <voice>"
+}
+
+func (advancedUser) Parent() core.CommandStatic {
+	return Advanced
+}
+
+func (advancedUser) Children() core.CommandsStatic {
+	return nil
+}
+
+func (advancedUser) Init() error {
+	return nil
+}
+
+func (c advancedUser) Run(m *core.Message) (any, error, error) {
+	if len(m.Command.Args) < 2 {
+		return m.Usage(), core.ErrMissingArgs, nil
+	}
+
+	switch m.Frontend {
+	case frontends.Discord:
+		return c.discord(m)
+	default:
+		return c.text(m)
+	}
+}
+
+func (c advancedUser) discord(m *core.Message) (*dg.MessageEmbed, error, error) {
+	voice, err := c.core(m)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	embed := &dg.MessageEmbed{
+		Description: "Added voice " + voice,
+	}
+
+	return embed, nil, nil
+}
+
+func (c advancedUser) text(m *core.Message) (string, error, error) {
+	voice, err := c.core(m)
+
+	if err != nil {
+		return "", nil, err
+	}
+
+	return "Added voice " + voice, nil, nil
+}
+
+func (advancedUser) core(m *core.Message) (string, error) {
+	user := m.Command.Args[0]
+	voice := m.Command.Args[1]
+
+	here, err := m.Here.ScopeLogical()
+	if err != nil {
+		return "", err
+	}
+
+	person, err := nick.ParsePersonHere(m, user)
+	if err != nil {
+		return "", err
+	}
+
+	return voice, UserVoiceSet(person, here, voice)
 }
