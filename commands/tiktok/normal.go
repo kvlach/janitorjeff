8edf4a5e124ea27a1f629c56171dbe123eefe585
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-02-25 15:37:52 +0200

    cmd/tiktok/normal: Add comment with command name

--- commands/tiktok/normal.go
+++ commands/tiktok/normal.go
@@ -4,6 +4,12 @@ import (
 	"github.com/janitorjeff/jeff-bot/core"
 )
 
+///////////
+//       //
+// voice //
+//       //
+///////////
+
 // NormalVoice is not a sub-command
 var NormalVoice = normalVoice{}
 
