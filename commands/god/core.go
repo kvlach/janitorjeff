6989023dabc5e6bdcc2dfc86f60ddbb3c8f02cfc
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-03-04 10:15:01 +0200

    cmd/god: Move db stuff into core.go
    No point in having a separate file for db stuff anymore as it would just
    include the schema.

--- commands/god/core.go
+++ commands/god/core.go
@@ -10,6 +10,19 @@ import (
 	gogpt "github.com/sashabaranov/go-gpt3"
 )
 
+const dbTablePlaceSettings = "CommandGodPlaceSettings"
+
+const dbSchema = `
+-- All settings must come with default values, as those are used when first
+-- adding a new entry.
+CREATE TABLE IF NOT EXISTS CommandGodPlaceSettings (
+	place INTEGER PRIMARY KEY,
+	reply_on BOOLEAN NOT NULL DEFAULT FALSE,
+	reply_interval INTEGER NOT NULL DEFAULT 1800, -- in seconds
+	reply_last INTEGER NOT NULL DEFAULT 0 -- unix timestamp of last reply
+);
+`
+
 var ErrIntervalTooShort = errors.New("The given interval is too short.")
 
 func Talk(prompt string) (string, error) {
