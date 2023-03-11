a1a8786ba94fdb0a400ff02e3f5a72bc162bec66
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-03-11 13:52:54 +0200

    core/frontend: Add Type function in interface

--- core/frontends.go
+++ core/frontends.go
@@ -4,6 +4,14 @@ import (
 	"sync"
 )
 
+type FrontendType int
+
 type Frontender interface {
+	// Type returns the frontend type ID.
+	Type() FrontendType
+
+	// Init is responsible for starting up any frontend specific services and
+	// connecting to frontend. When it receives the stop signal then it should
+	// disconnect from everything.
 	Init(wgInit, wgStop *sync.WaitGroup, stop chan struct{})
 }
