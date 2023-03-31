3e5cf6377bfc6b6c0af5f6afc06ab6819d1fd00a
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-03-31 15:25:16 +0300

    twitch: Use redis for scope caching

--- frontends/twitch/here.go
+++ frontends/twitch/here.go
@@ -1,5 +1,12 @@
 package twitch
 
+import (
+	"github.com/janitorjeff/jeff-bot/core"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog/log"
+)
+
 type Here struct {
 	RoomID   string
 	RoomName string
@@ -14,7 +21,22 @@ func (h Here) Name() string {
 }
 
 func (h Here) Scope() (int64, error) {
-	return dbAddChannelSimple(h.ID(), h.Name())
+	slog := log.With().Str("author", h.ID()).Logger()
+	rdbKey := "frontend_twitch_scope_" + h.ID()
+
+	scope, err := core.RDB.Get(ctx, rdbKey).Int64()
+	if err != redis.Nil {
+		slog.Debug().Int64("scope", scope).Msg("CACHE: found scope")
+		return scope, nil
+	}
+
+	scope, err = dbAddChannelSimple(h.ID(), h.Name())
+	if err != nil {
+		return -1, err
+	}
+	err = core.RDB.Set(ctx, rdbKey, scope, 0).Err()
+	slog.Debug().Err(err).Int64("scope", scope).Msg("CACHE: cached scope")
+	return scope, err
 }
 
 func (h Here) ScopeExact() (int64, error) {
