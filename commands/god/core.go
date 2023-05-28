2178e6d7e59c5e42ff4ba0dba0b75db75e95917a
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-05-28 21:44:43 +0300

    cmd/god: increase response length

--- commands/god/core.go
+++ commands/god/core.go
@@ -22,7 +22,7 @@ func Talk(prompt string) (string, error) {
 
 	req := gogpt.CompletionRequest{
 		Model:     gogpt.GPT3TextDavinci003,
-		MaxTokens: 40,
+		MaxTokens: 60,
 		Prompt:    prompt,
 	}
 	resp, err := c.CreateCompletion(ctx, req)
