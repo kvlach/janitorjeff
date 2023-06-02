146a466e7a77121bfb743a1759ca26a538de1914
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-06-02 18:54:09 +0300

    core/cmd: Add entire transaction in func

--- commands/god/advanced.go
+++ commands/god/advanced.go
@@ -74,19 +74,42 @@ func (advanced) Init() error {
 			return
 		}
 
-		if on, err := ReplyOnGet(here); err != nil || !on {
-			log.Debug().Err(err).Msg("reply not on, skipping")
-			return
-		}
-
-		if should, err := ShouldReply(here); err != nil || !should {
-			log.Debug().Err(err).Msg("shouldn't reply yet, skipping")
-			return
-		}
-
 		// GPT takes a couple of seconds to produce a response which in turn
 		// makes the whole bot lag
 		go func() {
+			core.DB.Lock.Lock()
+			defer core.DB.Lock.Unlock()
+
+			tx, err := core.DB.Begin()
+			if err != nil {
+				log.Error().Err(err).Msg("failed to begin transaction")
+				return
+			}
+			//goland:noinspection GoUnhandledErrorResult
+			defer tx.Rollback()
+
+			if on, err := tx.PlaceGet("cmd_god_reply_on", here).Bool(); err != nil || !on {
+				log.Error().Err(err).Msg("reply not on, skipping")
+				return
+			}
+
+			interval, err := tx.PlaceGet("cmd_god_reply_interval", here).Duration()
+			if err != nil {
+				return
+			}
+			last, err := tx.PlaceGet("cmd_god_reply_last", here).Time()
+			if err != nil {
+				return
+			}
+			diff := time.Now().UTC().Sub(last)
+			if interval > diff {
+				log.Debug().
+					Interface("interval", interval).
+					Interface("diff", diff).
+					Msg("shouldn't reply yet, skipping")
+				return
+			}
+
 			resp, err := Talk(m.Raw)
 			if err != nil {
 				log.Debug().Err(err).Msg("failed to communicate with god")
@@ -98,9 +121,13 @@ func (advanced) Init() error {
 				return
 			}
 
-			if err := ReplyLastSet(here, time.Now()); err != nil {
+			if err := tx.PlaceSet("cmd_god_reply_last", here, time.Now().UTC().Unix()); err != nil {
 				log.Debug().Err(err).Msg("error while trying to set reply")
 				return
+			}
+
+			if err := tx.Commit(); err != nil {
+				log.Error().Err(err).Msg("couldn't commit")
 			}
 		}()
 	})
