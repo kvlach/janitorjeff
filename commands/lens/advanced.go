308eb829b6af7a37f3f7ae7108c8f45a44868618
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-06-08 11:15:36 +0300

    feat>cmd/lens: director delete subcmd

--- commands/lens/advanced.go
+++ commands/lens/advanced.go
@@ -108,6 +108,7 @@ func (advancedDirectors) Parent() core.CommandStatic {
 func (advancedDirectors) Children() core.CommandsStatic {
 	return core.CommandsStatic{
 		AdvancedDirectorsAdd,
+		AdvancedDirectorsDelete,
 	}
 }
 
@@ -210,4 +211,97 @@ func (advancedDirectorsAdd) core(m *core.Message) (string, error) {
 	// exists between each word.
 	name := strings.Join(m.Command.Args, " ")
 	return name, DirectorAdd(name)
+}
+
+//////////////////////
+//                  //
+// directors delete //
+//                  //
+//////////////////////
+
+var AdvancedDirectorsDelete = advancedDirectorsDelete{}
+
+type advancedDirectorsDelete struct{}
+
+func (c advancedDirectorsDelete) Type() core.CommandType {
+	return c.Parent().Type()
+}
+
+func (c advancedDirectorsDelete) Permitted(m *core.Message) bool {
+	return c.Parent().Permitted(m)
+}
+
+func (advancedDirectorsDelete) Names() []string {
+	return core.AliasesDelete
+}
+
+func (advancedDirectorsDelete) Description() string {
+	return "Delete a director from the list of directors being monitored for new releases."
+}
+
+func (advancedDirectorsDelete) UsageArgs() string {
+	return "<name>"
+}
+
+func (c advancedDirectorsDelete) Category() core.CommandCategory {
+	return c.Parent().Category()
+}
+
+func (advancedDirectorsDelete) Examples() []string {
+	return nil
+}
+
+func (advancedDirectorsDelete) Parent() core.CommandStatic {
+	return AdvancedDirectors
+}
+
+func (advancedDirectorsDelete) Children() core.CommandsStatic {
+	return nil
+}
+
+func (advancedDirectorsDelete) Init() error {
+	return nil
+}
+
+func (c advancedDirectorsDelete) Run(m *core.Message) (any, error, error) {
+	if len(m.Command.Args) < 1 {
+		return m.Usage(), core.ErrMissingArgs, nil
+	}
+
+	switch m.Frontend.Type() {
+	case discord.Frontend.Type():
+		return c.discord(m)
+	default:
+		return c.text(m)
+	}
+}
+
+func (c advancedDirectorsDelete) discord(m *core.Message) (*dg.MessageEmbed, error, error) {
+	name, err := c.core(m)
+	if err != nil {
+		return nil, nil, err
+	}
+	embed := &dg.MessageEmbed{
+		Description: c.fmt(name),
+	}
+	return embed, nil, nil
+}
+
+func (c advancedDirectorsDelete) text(m *core.Message) (string, error, error) {
+	name, err := c.core(m)
+	if err != nil {
+		return "", nil, err
+	}
+	return c.fmt(name), nil, nil
+}
+
+func (advancedDirectorsDelete) fmt(name string) string {
+	return "No longer monitoring the director " + name
+}
+
+func (advancedDirectorsDelete) core(m *core.Message) (string, error) {
+	// Use strings.Join instead of m.RawArgs to ensure that only one space
+	// exists between each word.
+	name := strings.Join(m.Command.Args, " ")
+	return name, DirectorDelete(name)
 }
