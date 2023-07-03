253c97d4cf7e0326678df4cb4c0efc62d1224aac
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-07-03 11:51:30 +0300

    core: new files for Here and Author
    No longer makes sense to have them in the Message event since they are used in other events as well

---
+++ core/author.go
@@ -0,0 +1,41 @@
+package core
+
+// Author is the interface used to abstract a frontend's message author.
+type Author interface {
+	// ID returns the author's ID, this should be a unique, static, identifier
+	// in that frontend.
+	ID() (string, error)
+
+	// Name returns the author's username.
+	Name() (string, error)
+
+	// DisplayName return's the author's display name. If only usernames exist
+	// for that frontend then returns the username.
+	DisplayName() (string, error)
+
+	// Mention return's a string that mention's the author. This should ideally
+	// ping them in some way.
+	Mention() (string, error)
+
+	// BotAdmin returns true if the author is a bot admin, otherwise returns
+	// false.
+	BotAdmin() (bool, error)
+
+	// Admin checks if the author is considered an admin. Should return true
+	// only if the author has basically every permission.
+	Admin() (bool, error)
+
+	// Moderator checks if the author is considered a moderator. General rule of
+	// thumb is that if the author can ban people, then they are mods.
+	Moderator() (bool, error)
+
+	// Subscriber returns true if the author is considered a subscriber. General
+	// rule of thumb is that if they are paying money in some way, then they are
+	// a subscriber. If no such thing exists for the specific frontend, then it
+	// should always return false.
+	Subscriber() (bool, error)
+
+	// Scope return's the author's scope. If it doesn't exist it will create it
+	// and add it to the database.
+	Scope() (author int64, err error)
+}
